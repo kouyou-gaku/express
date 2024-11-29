33f750d22e67cfa946e4965078073ea64241340f
kouyou-gaku <2201100308op@cyber-u.ac.jp>
2024-11-29 20:06:51 +0900

    add NOde

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
 app.use('/cat', catRouter);
-app.use('/Node', catRouter);
+app.use('/Node', NodeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
