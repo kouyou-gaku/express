06f7d0fe1bedad034df0bc6c4c9f2e03e0393180
kouyou-gaku <2201100308op@cyber-u.ac.jp>
2025-01-28 14:52:10 +0900

    www

--- routes/notes.js
+++ routes/notes.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 const { MongoClient } = require("mongodb");
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
+
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
 const database = client.db('notes');
